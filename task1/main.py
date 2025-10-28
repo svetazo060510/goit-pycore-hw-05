efe716f4501f9887c070dc04c694351471f9c5bf
Svitlana Stremedlovska <svetazo060510@gmail.com>
2025-10-28 19:25:09 +0200

    task1

---
+++ task1/main.py
@@ -0,0 +1,34 @@
+from typing import Callable
+
+def caching_fibonacci() -> Callable[[int], int]:
+    cache = {}
+
+    def fibonacci(n: int) -> int:
+        if n <= 0:
+            return 0
+        if n == 1:
+            return 1
+        
+        if n in cache:
+            return cache[n]  
+
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+
+    return fibonacci
+
+# -- Приклад використання --
+# Отримуємо функцію fibonacci, яка "пам'ятає" свій кеш
+fib = caching_fibonacci()
+
+# Використовуємо функцію fibonacci для обчислення чисел
+# При першому виклику fib(10), вона обчислить і закешує
+# fib(1) ... fib(10).
+print(f"fib(10) = {fib(10)}")  # Виведе 55
+
+# При виклику fib(15), вона використає вже збережені
+# значення від 1 до 10 і обчислить лише fib(11) ... fib(15).
+print(f"fib(15) = {fib(15)}")  # Виведе 610
+
+# Цей виклик буде миттєвим, оскільки fib(10) вже є в кеші
+print(f"fib(10) = {fib(10)}")  # Виведе 55
